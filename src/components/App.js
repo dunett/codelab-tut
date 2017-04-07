65e98f5e54c9629b5990de063ed2b4d750a219d3
dunett <dunett@nate.com>
2017-04-07 16:57:23 +0900

    전화번호부 선택

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
+import Contact from './Contact';
 
-class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            name: ''
-        };
-    }
+class App extends React.Component {
     render(){
-        return(
-            <div>
-                <button onClick={()=>{this.setState({name: 'Veloper'})}}>Click Me</button>
-                <h1>Hello!! {this.state.name}</h1>
-            </div>
-        )
+        return (
+                <Contact/>
+        );
     }
 }
 
